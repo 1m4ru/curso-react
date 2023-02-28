d6c04502e81d7a3371f6534911130d3a0162301c
Maru <maruanibrahim22@gmail.com>
2023-02-28 16:27:42 -0300

    :refatoração final da hora em JS no react

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,43 @@
-import {format, formatDistanceToNow} from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR'
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { Avatar } from "./avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishiedAt }) {
- const publishedDateFormatted = format(publishiedAt, "d 'de' LLLL  'às' HH:mm'h'", {
-  locale: ptBR,
- } )
+  const publishedDateFormatted = format(
+    publishiedAt,
+    "d 'de' LLLL  'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
- const publishedDateRelativeToNow = formatDistanceToNow(publishiedAt, {
+  const publishedDateRelativeToNow = formatDistanceToNow(publishiedAt, {
     locale: ptBR,
     addSuffix: true,
- })
+  });
 
   return (
     <article className={styles.Post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={author.avatarUrl}
-          />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
             <strong>{author.name}</strong>
             <span>{author.role}</span>
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime="2022-05-11 08:13:08">
-        {publishedDateRelativeToNow}
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishiedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
-      <div className={styles.content}>
-        
-      </div>
+      <div className={styles.content}></div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
