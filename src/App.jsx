14739261f8cdcb3472495b60ba74b47c1f413041
Maru <maruanibrahim22@gmail.com>
2023-02-28 14:27:43 -0300

    iniciando toda a refatoração do autor dos posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,33 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar"
 import "./global.css";
 import styles from "./app.module.css";
+
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+//content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/1m4ru.png',
+      name: 'Maruan Moussa',
+      role: 'Dev Front End'
+    },
+    content: [
+       'Fala galeraa 👋', 
+        
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz',
+          'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+          '👉{" "}',
+          <a href=""> jane.design/doctorcare </a>
+          <a href=""> #novoprojeto </a> 
+          <a href="">#nlw </a>{" "}
+           #rocketseat
+        
+    ],
+  },
+]
 function App() {
   return (
     <div>
