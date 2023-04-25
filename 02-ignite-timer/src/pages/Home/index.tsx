8cf41b31adddacc8d6f1f8b47af2d215a654a3c1
Maru <maruanibrahim22@gmail.com>
2023-04-25 10:08:59 -0300

    :finalizando a parte de estilização do app

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -7,10 +7,22 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" />
+          <TaskInput id="task" list="task-suggestion" placeholder="Dê um nome para o seu projeto" />
+
+        <datalist id="task-suggestion">
+          <option value="Projeto 1" />
+          <option value="Projeto 2" />
+          <option value="Projeto 3" />
+        </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput
+           type="number" 
+           id="minutesAmount"  
+           placeholder="00"
+           step={5}
+           min={0}
+           />
 
           <span>minutos.</span>
         </FormContainer>
--- 02-ignite-timer/src/pages/Home/styles.ts
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
@@ -29,8 +29,35 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `;
 
-export const TaskInput = styled.input ``;
-export const MinutesAmountInput = styled.input ``;
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-family: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator{
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
 
 export const CountdownContainer = styled.div`
   font-family: "Roboto Mono", monospace;
@@ -41,48 +68,45 @@ export const CountdownContainer = styled.div`
   display: flex;
   gap: 1rem;
 
-   span{
-    background: ${(props) => props.theme["gray-700"]}; 
-    padding: 2rem 1rem ;
+  span {
+    background: ${(props) => props.theme["gray-700"]};
+    padding: 2rem 1rem;
     border-radius: 8px;
-   }
+  }
 `;
 
-export const Separator= styled.div`
-    padding: 2rem 0;
-    color: ${(props) => props.theme["green-500"]};
-    
-    width: 4rem;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    
-    
-    `;
+export const Separator = styled.div`
+  padding: 2rem 0;
+  color: ${(props) => props.theme["green-500"]};
 
-export const StartCountdownButton = styled.button `
-    width: 100%;
-    border: 0;
-    padding: 1rem;
-    border-radius: 8px;
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
 
-    gap: 0.5rem;
-    font-weight: bold;
-    cursor: pointer;
-    background: ${(props) => props.theme["green-500"]};
-    color: ${(props) => props.theme["gray-100"]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    &:disabled {
-      opacity: 0.7;
-      cursor: not-allowed;
-    }
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
 
-    &:not(:disabled):hover {
-      background: ${(props) => props.theme["green-700"]};
-    }
-`;    
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
 
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
+`;
